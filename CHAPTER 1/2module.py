ce3b405f831930931cbcb043f040a07549acac08
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2024-11-20 15:14:11 +0530

    Chapter 1, Hello world

---
+++ CHAPTER 1/2module.py
@@ -0,0 +1,22 @@
+# Module -> A file containing code, made by someone else. we can use it using (pip install)
+
+import pyjokes
+
+# this prints a random joke
+jokes = pyjokes.get_joke()
+print(jokes)
+
+
+#PIP -> Package manager for python, used to install module
+
+#Module -> inbuilt and external
+
+#REPL -> we can use it on terminal Read Evaluate Print Loop
+
+"""THis is 
+a multiline 
+comment"""
+
+'''Also
+a single line
+comment'''
